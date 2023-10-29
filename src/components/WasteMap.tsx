20a286565cd52be15daaa17949b555f0c5e1a7e5
KLAKALU <player4797462226@gmail.com>
2023-10-29 10:29:25 +0900

    add: fetch data from supabase

--- src/components/WasteMap.tsx
+++ src/components/WasteMap.tsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect } from "react";
 import { Map } from "./Map";
 import currentMarker from "../assets/map-pin-svgrepo-com.svg";
 import { Position } from "../types/position"
-
+import supabase from "./Supabase"
 
 export default function WasteMap() {
   const [currentPosition, setCurrentPosition] = useState<Position>({lat: 0, lng: 0});
@@ -23,6 +23,20 @@ export default function WasteMap() {
       console.error("Error: Geolocation is not supported");
     }
   }, []);
+
+  
+
+  async function fetchData() {
+    try {
+      const { data, error } = await supabase.from("trash_can_location").select();
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchData();
 
   const { isLoaded, onLoad } = Map();
 
